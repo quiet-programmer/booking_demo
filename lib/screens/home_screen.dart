4bb7b1212ac6e75eafd155d44f0dec932f47b1aa
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-12-11 12:31:41 +0100

    added screen shot and little changes to home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -72,7 +72,7 @@ class _HomeScreenState extends State<HomeScreen> {
                           });
                         },
                         decoration: InputDecoration(
-                          hintText: "Search user name only",
+                          hintText: "Search name only",
                           disabledBorder: InputBorder.none,
                           enabledBorder: InputBorder.none,
                           focusedBorder: InputBorder.none,
